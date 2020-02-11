f00829909887b8729ae860d2ef91b37ed40bf720
Stuart Dambi <stuartdambi@witsmind.com>
2020-02-11 23:08:07 +0300

    added BookType schema

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    
+}));
 
 
 const PORT = process.env.PORT || 5000;
---
+++ schema/schema.js
@@ -0,0 +1,14 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({});
